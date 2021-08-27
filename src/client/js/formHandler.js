853ea842f4d2bb441d46345acce4da84986b2762
Abdalla Mush0 <abdalla.magdy@gmail.com>
2021-08-27 17:06:18 +0200

    server send proper reply/client received it successfuly

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,23 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
+  checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  console.log("::: Form Submitted :::");
+  fetch("http://localhost:8080/test", {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+    .then((res) => res.json())
+    .then(function (res) {
+      document.getElementById("results").innerHTML = res.subjectivity;
+    });
 }
 
-export { handleSubmit }
+// export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,28 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const express = require("express");
+require("dotenv").config();
+const mockAPIResponse = require("./mockAPI");
+var cors = require("cors");
 
-const app = express()
+// console.log(process.env.API_key);
 
-app.use(express.static('dist'))
+var app = express();
 
-console.log(__dirname)
+app.use(cors());
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+console.log(path.join(__dirname, "../client"));
+app.use("/static", express.static(path.join(__dirname, "../client")));
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log("Example app listening http://127.0.0.1:8080/");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
