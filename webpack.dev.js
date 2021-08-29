869f0681fb5744c70ce8390bdd0c983e2e617d28
Abdalla Mush0 <abdalla.magdy@gmail.com>
2021-08-29 10:49:40 +0200

    clean-webpack-plugin works properly,webpack-dev-server runs on port:3000

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -9,6 +9,9 @@ module.exports = {
   mode: "development",
   devtool: "source-map",
   stats: "verbose",
+  devServer: {
+    port: 3000,
+  },
   output: {
     library: {
       name: "Client",
@@ -33,15 +36,6 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
       favicon: "./src/client/imgs/news-icon.png",
-    }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
   ],
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,15 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
   output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.[contenthash].js",
     library: {
       name: "Client",
       type: "var",
@@ -33,6 +36,15 @@ module.exports = {
       favicon: "./src/client/imgs/news-icon.png",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: false,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
     new GenerateSW(),
   ],
 };
